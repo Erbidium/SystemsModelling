a11afdff598355d257baca04a9202f93061ffa7d
Erbidium <OlexandrTanasienko@gmail.com>
2023-09-19 00:19:07 +0300

    Implement third generator

---
+++ Lab1/Lab1/Lab1/Generators/UniformDistributionGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab1.Generators;
+
+public class UniformDistributionGenerator : IGenerator
+{
+    private double _z0 = 3;
+
+    private const double A = 5e13;
+
+    private const double C = 2e31;
+
+    private double _currentZ;
+
+    public UniformDistributionGenerator()
+    {
+        _currentZ = _z0;
+    }
+    
+    public double GenerateNumber()
+    {
+        return A * (_currentZ % C) / C;
+    }
+}
