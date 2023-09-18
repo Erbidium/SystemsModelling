fd5c9f5e8eed70e56a9e983759eddf4d171c8c1b
Erbidium <OlexandrTanasienko@gmail.com>
2023-09-19 00:24:51 +0300

    Create number sequence generator

---
+++ Lab1/Lab1/Lab1/Generators/NumberSequenceGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab1.Generators;
+
+public class NumberSequenceGenerator
+{
+    private IGenerator _generator;
+
+    public NumberSequenceGenerator(IGenerator generator)
+    {
+        _generator = generator;
+    }
+    
+    public List<double> GenerateSequence(int numbersCount)
+    {
+        List<double> generatedNumbers = new();
+        for (int i = 0; i < numbersCount; i++)
+        {
+            generatedNumbers.Add(_generator.GenerateNumber());
+        }
+
+        return generatedNumbers;
+    }
+}
