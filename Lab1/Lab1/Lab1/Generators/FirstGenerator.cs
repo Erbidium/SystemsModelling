f3366638fa9adb0cdb75d0b5729b82d448d1cca7
Erbidium <OlexandrTanasienko@gmail.com>
2023-09-18 23:51:07 +0300

    Implement first generator

---
+++ Lab1/Lab1/Lab1/Generators/FirstGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1.Generators;
+
+public class FirstGenerator : IGenerator
+{
+    private double _lambda;
+
+    private Random _random = new();
+    
+    public FirstGenerator(double lambda)
+    {
+        _lambda = lambda;
+    }
+    
+    public double GenerateNumber()
+    {
+        return -1 / _lambda * Math.Log(_random.NextDouble());
+    }
+}
