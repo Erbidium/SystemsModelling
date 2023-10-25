caf6ea7f6640632c389be47cc00cf077e865e4e5
Erbidium <OlexandrTanasienko@gmail.com>
2023-10-25 17:05:15 +0300

    Create model with multiple processes

--- Lab2/Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Lab2/Program.cs
@@ -2,24 +2,20 @@
 using Lab2.Delays;
 using Lab2.Elements;
 
+const int simulationTime = 1000;
+
 var createDelay = new ExponentialDelay(2);
 var processDelay = new ExponentialDelay(1);
 
-var process = new Process(processDelay)
-{
-    Name = "PROCESSOR",
-    MaxQueue = 5
-};
+var create = new Create(createDelay) { Name = "CREATOR" };
 
-var create = new Create(createDelay)
-{
-    Name = "CREATOR",
-    NextElement = process
-};
+var process1 = new Process(1, processDelay) { Name = "PROCESSOR1", MaxQueue = 5 };
+var process2 = new Process(1, processDelay) { Name = "PROCESSOR2", MaxQueue = 5 };
+var process3 = new Process(1, processDelay) { Name = "PROCESSOR3", MaxQueue = 5 };
 
-Console.WriteLine($"Id0 = {create.Id}  Id1 = {process.Id}");
+create.NextElement = process1;
+process1.NextElement = process2;
+process2.NextElement = process3;
 
-var elements = new List<Element>{ create, process };
-
-var model = new Model(elements);
-model.Simulate(1000);
+var model = new Model(new (){ create, process1, process2, process3 });
+model.Simulate(simulationTime);
