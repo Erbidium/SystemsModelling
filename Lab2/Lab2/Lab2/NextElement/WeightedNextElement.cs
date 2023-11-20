e1daf3e58a882ee16cffb7779137569f9e0d870a
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-20 19:41:57 +0200

    Implement weighted next elements logic

--- Lab2/Lab2/Lab2/NextElement/WeightedNextElement.cs
+++ Lab2/Lab2/Lab2/NextElement/WeightedNextElement.cs
@@ -4,10 +4,28 @@ namespace Lab2.NextElement;
 
 public class WeightedNextElement : INextElement
 {
-    public WeightedNextElement(Element nextElement)
-    {
-        NextElement = nextElement;
-    }
+
+    public List<(Element Element, double Chance)> NextElementChances = new();
     
-    public Element? NextElement { get; }
+    private readonly Random _rand = new();
+
+    public Element? NextElement
+    {
+        get
+        {
+            double totalChancesSum = NextElementChances.Sum(el => el.Chance);
+            double chanceGeneratedValue = _rand.NextDouble() * totalChancesSum;
+            
+            double chancesAccumulatedSum = 0;
+            for (int i = 0; i < NextElementChances.Count; i++)
+            {
+                chancesAccumulatedSum += NextElementChances[i].Chance;
+                if (chancesAccumulatedSum > chanceGeneratedValue)
+                {
+                    return NextElementChances[i].Element;
+                }
+            }
+            return null;
+        }
+    }
 }
