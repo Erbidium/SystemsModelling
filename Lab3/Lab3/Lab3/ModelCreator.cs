501d338d8d75bbbc5ef516664d70de96a4ff776f
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-28 18:36:57 +0200

    Add temp hospital model

--- Lab3/Lab3/Lab3/ModelCreator.cs
+++ Lab3/Lab3/Lab3/ModelCreator.cs
@@ -35,6 +35,26 @@ public static class ModelCreator
 
     public static NetMO CreateHospitalModel()
     {
+        var createDelay = new ExponentialDelay(0.5);
+        var processDelay = new ExponentialDelay(0.3);
+
+        var create = new Create(createDelay) { Name = "PATIENTS_CREATOR", TimeNext = 0.1 };
+
+        var smo1 = new SystemMO(processDelay, 1) { Name = "CASHIER1" };
+        var smo2 = new SystemMO(processDelay, 1) { Name = "CASHIER2" };
+
+        smo1.Devices[0].IsServing = true;
+        smo1.Devices[0].TimeNext = new NormalDelay(1, 0.3).Generate();
         
+        smo2.Devices[0].IsServing = true;
+        smo2.Devices[0].TimeNext = new NormalDelay(1, 0.3).Generate();
+
+        create.NextElement = new PriorityNextElementPicker(new List<(Element Element, int Priority)> { (smo1, 2), (smo2, 1) });
+
+        var elements = new List<Element> { create, smo1, smo2 };
+        smo1.Queue = new BankQueue(new []{ new SimpleItem(), new SimpleItem()}, 3) { Elements = elements };
+        smo2.Queue = new BankQueue(new []{ new SimpleItem(), new SimpleItem()}, 3) { Elements = elements };
+
+        return new NetMO(elements);
     }
 }
--- Lab3/Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Lab3/Program.cs
@@ -3,10 +3,10 @@ using Lab3.ModelStats;
 
 const int simulationTime = 1000;
 
-var model = ModelCreator.CreateShopModel();
 //var model = ModelCreator.CreateHospitalModel();
-
 //var statsPrinter = new BankModelStats(model);
+
+var model = ModelCreator.CreateHospitalModel();
 var statsPrinter = new HospitalModelStats(model);
 
 model.Simulate(simulationTime, statsPrinter);
