f01500ae20ebc219a0ff3546638c2aaf2ebe76b2
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-28 16:37:44 +0200

    Implement uniform delay

---
+++ Lab3/Lab3/Lab3/Delays/UniformDelay.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab3.Delays;
+
+public class UniformDelay : IDelay
+{
+    private readonly Random _random = new();
+
+    private readonly double _min;
+    private readonly double _max;
+
+    public UniformDelay(double min, double max)
+    {
+        _max = max;
+        _min = min;
+    }
+    
+    public double Generate()
+    {
+        return _min + _random.NextDouble() * (_max - _min);
+    }
+}
