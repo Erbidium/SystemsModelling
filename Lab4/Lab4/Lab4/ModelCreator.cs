96b59cbc2f8bb6a70d99f7fbb7402b6e7aba146f
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-29 21:23:47 +0200

    Create branched and chained models

--- Lab4/Lab4/Lab4/ModelCreator.cs
+++ Lab4/Lab4/Lab4/ModelCreator.cs
@@ -26,11 +26,81 @@ public static class ModelCreator
             
             process.Enter(new SimpleItem());
             
-            previousElement.NextElement = new OneNextElementPicker(process);
+            previousElement.NextElement = new WeightedNextElementPicker
+            {
+                NextElementChances = { (process, 1) }
+            };
+            
             previousElement = process;
             
             elements.Add(process);
         }
+
+        return new NetMO(elements);
+    }
+    
+    public static NetMO CreateBranchedModel(int processesCount, int branchesCount)
+    {
+        var delay = new ExponentialDelay(0.5);
+        var simpleItemFactory = new SimpleItemFactory();
+        
+        var create = new Create(delay, simpleItemFactory) { Name = "CREATOR" };
+
+        var elements = new List<Element> { create };
+
+        var branches = new Element[branchesCount];
+        for (int i = 0; i < branchesCount; i++)
+        {
+            branches[i] = new SystemMO(delay, 1) { Name = $"PROCESS{i + 1}"};
+            elements.Add(branches[i]);
+        }
+
+        create.NextElement = new WeightedNextElementPicker
+        {
+            NextElementChances = branches.Select(b => (b, 1.0)).ToList()
+        };
+        
+        int createdProcessesCount = branchesCount;
+
+        for (int i = 0; i < branchesCount; i++)
+        {
+            for (int j = 0; j < (processesCount - branchesCount) / branchesCount; j++)
+            {
+                var process = new SystemMO(delay, 1){ Name = $"PROCESS{createdProcessesCount + 1}" };
+                elements.Add(process);
+                
+                branches[i].NextElement = new WeightedNextElementPicker
+                {
+                    NextElementChances = { (process, 1) }
+                };
+                branches[i] = process;
+                
+                createdProcessesCount++;
+                if (createdProcessesCount == processesCount)
+                {
+                    goto exitLoop;
+                }
+            }
+        }
+        
+        exitLoop:
+
+        int remainingProcessesCount = (processesCount - branchesCount) % branchesCount;
+        for (int i = 0; i < remainingProcessesCount; i++)
+        {
+            var process = new SystemMO(delay, 1){ Name = $"PROCESS{createdProcessesCount + 1}" };
+            elements.Add(process);
+                
+            branches[i].NextElement = new WeightedNextElementPicker
+            {
+                NextElementChances = { (process, 1) }
+            };
+            branches[i] = process;
+                
+            createdProcessesCount++;
+        }
+        
+        Console.WriteLine($"Created count: {createdProcessesCount}");
 
         return new NetMO(elements);
     }
--- Lab4/Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Lab4/Program.cs
@@ -3,5 +3,6 @@ using Lab3.ModelStats;
 
 const int simulationTime = 10000;
 
-var model = ModelCreator.CreateChainedModel(500);
+//var model = ModelCreator.CreateChainedModel(100);
+var model = ModelCreator.CreateBranchedModel(23, 4);
 model.Simulate(simulationTime);
